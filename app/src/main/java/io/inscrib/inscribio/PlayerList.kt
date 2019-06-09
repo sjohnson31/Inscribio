27288aff057c9951c66685a266e69c1888fe0653
Sam Burba <gitlab@samburba.com>
2019-06-08 21:20:20 -0400

    Remove some useless comments
    And remove a useless method

--- app/src/main/java/io/inscrib/inscribio/PlayerList.kt
+++ app/src/main/java/io/inscrib/inscribio/PlayerList.kt
@@ -5,14 +5,7 @@ import java.util.HashMap
 
 object PlayerList {
 
-    /**
-     * An array of sample (dummy) items.
-     */
     val ITEMS: MutableList<Player> = ArrayList()
-
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
     private val ITEM_MAP: MutableMap<Int, Player> = HashMap()
 
     private const val COUNT = 5
@@ -33,10 +26,5 @@ object PlayerList {
         return Player(position, 0)
     }
 
-    /**
-     * A dummy item representing a piece of content.
-     */
-    data class Player(val id: Int, var score: Int) {
-        override fun toString(): String = score.toString()
-    }
+    data class Player(val id: Int, var score: Int)
 }
